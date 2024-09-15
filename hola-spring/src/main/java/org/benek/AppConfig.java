e0e1aee41281f5e66cb37ca73cced9872140a0eb
chiquillo7777 <carlosjosechiquillolopez@gmail.com>
2024-09-15 17:13:45 -0600

    second commit

---
+++ hola-spring/src/main/java/org/benek/AppConfig.java
@@ -0,0 +1,12 @@
+package org.benek;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public HolaMundo holaMundo(){
+        return new HolaMundo();
+    }
+}
